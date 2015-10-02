c9d61d67455631f768501de9033bf11c9667e2d6
Stephan Goeppentin <dedda102@gmail.com>
2015-10-02 13:14:29 +0200

    added app

---
+++ app/src/androidTest/java/kasus/dedda/org/morningtourette/ApplicationTest.java
@@ -0,0 +1,13 @@
+package kasus.dedda.org.morningtourette;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
